c5297dabb4dbe103b4a742cd1620f17152e4e8f6
fz <130398020+Fantastic1942@users.noreply.github.com>
2024-04-19 16:14:27 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -1,34 +1,17 @@
 #!/bin/sh
 
+# --
+# FantaHope
+# --
+
 # 获取系统信息
-distro=$(grep '^NAME=' /etc/os-release | sed 's/NAME=//; s/"//g')
-version=$(grep '^VERSION_ID=' /etc/os-release | sed 's/VERSION_ID=//; s/"//g')
-echo "Detected Linux distribution: $distro"
-echo "Version: $version"
-
-# 根据系统信息执行更新命令
-case $distro in
-    "Ubuntu")
-        echo "Updating Ubuntu system..."
-        sudo apt update
-        sudo apt upgrade -y
-        sudo apt autoremove -y
-        sudo apt autoclean
-        ;;
-    "CentOS Linux")
-        echo "Updating CentOS system..."
-        sudo yum update -y
-        ;;
-    "Alpine Linux")
-        echo "Updating Alpine system..."
-        sudo apk update
-        sudo apk upgrade
-        ;;
-    *)
-        echo "Unsupported Linux distribution: $distro"
-        exit 1
-        ;;
-esac
-
-echo "System update completed."
+echo "系统信息："
+echo "内核名称：" $(uname -s)
+echo "主机名：" $(uname -n)
+echo "内核版本：" $(uname -r)
+echo "操作系统类型：" $(uname -o)
+echo "发行版：" $(cat /etc/*-release | grep '^PRETTY_NAME=' | cut -d= -f2 | tr -d '"')
+echo "架构：" $(uname -m)
+echo "已登录用户：" $(whoami)
+echo "当前路径：" $(pwd)
 
